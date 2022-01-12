2ad1589f9ba8ce006796a2f0c41e7179394eacec
carry <carry@qq.com>
2022-01-12 10:38:30 +0800

    1

--- glory-utils/src/main/java/com/glory/gloryUtils/Main2.java
+++ glory-utils/src/main/java/com/glory/gloryUtils/Main2.java
@@ -1,13 +1,6 @@
 package com.glory.gloryUtils;
 
-import com.glory.gloryUtils.utils.ByteUtil;
-
 public class Main2 {
-    public static void main(String[] args) throws Exception {
-        String str = "\\xe4\\xb8\\xad\\xe5\\x9b\\xbd";
-        byte[] bytes = new byte[]{(byte) 0xe4, (byte) 0xb8, (byte) 0xad, (byte) 0xe5, (byte) 0x9b, (byte) 0xbd};
-        System.err.println(new String("\\xe4\\xb8\\xad\\xe5\\x9b\\xbd"));
-        System.err.println(ByteUtil.bytesToHex("中国".getBytes()));
-
+    public static void main(String[] args) {
     }
 }
